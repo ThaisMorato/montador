d8d3aecc19f51989de66680174f53f3f00470d14
henesaud <hstuchisaud@gmail.com>
2021-07-20 20:46:12 -0300

    Added instruction class and Symbols enum

--- include/montador.h
+++ include/montador.h
@@ -4,6 +4,7 @@
 //includes and namespaces
 #include <fstream>
 #include <vector>
+#include <map>
 using namespace std;
 
 //definitions
@@ -20,6 +21,32 @@ using namespace std;
 //         command = commandValue;
 //     }
 // };
+
+enum Symbol {
+    Halt = 0,  // Stop the program.
+    Load = 1,  // Reg[R] <- Mem[M + PC]
+    Store = 2, // Mem[M + PC] <- Reg[R]
+    Read = 3,  // Reg[R] <- input
+    Write = 4, // output <- Reg[R]
+    Copy = 5,  // Reg[R1] <- Reg[R2], update PSW
+    Push = 6,  // SP <- SP - 1; Mem[SP] <- Reg[R]
+    Pop = 7,   // Reg[R] <- Mem[SP]; SP <- SP + 1;
+    Add = 8,   // Reg[R1] <- Reg[R1] + Reg[R2], update PSW
+    Sub = 9,   // Reg[R1] <- Reg[R1] - Reg[R2], update PSW
+    Mul = 10,  // Reg[R1] <- Reg[R1] * Reg[R2], update PSW
+    Div = 11,  // Reg[R1] <- Reg[R1] / Reg[R2], update PSW
+    Mod = 12,  // Reg[R1] <- Reg[R1] % Reg[R2], update PSW
+    And = 13,  // Reg[R1] <- Reg[R1] & Reg[R2], update PSW
+    Or = 14,   // Reg[R1] <- Reg[R1] | Reg[R2], update PSW
+    Not = 15,  // Reg[R1] <- ~Reg[R1], update PSW
+    Jump = 16, // PC <- PC + M
+    JZ = 17,   // If PSW[0] == 1, PC <- PC + M
+    JN = 18,   // If PSW[1] == 1, PC <- PC + M
+    Call = 19, // SP <- SP - 1; Mem[SP] <- PC; PC <- PC + M
+    Ret = 20,  // PC <- Mem[SP]; SP <- SP + 1
+};
+
+
 class Program {
     public:
     vector<vector<string>> lines;
@@ -33,9 +60,20 @@ class Program {
     }
 };
 
+
+//class to acomodate an instruction
+class Intruction {
+    public:
+	Symbol sym;
+    vector<int> operands; // 0, 1, or 2 operands, depending on the symbol <sym>.
+};
+
+
+
+
 //assembler functions
-int assemble(char *fileName);
-Program readProgram(ifstream &programFile);
-vector<string> getMeaningfulVec(string &line);
+int assemble(char *fileName);  					//called in main
+Program readProgram(ifstream &programFile);		//read a file (programFile) and return a  Program instance
+vector<string> getMeaningfulVec(string &line);	
 
 #endif

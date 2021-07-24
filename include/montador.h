1fe5ec308d629f6ddcc71bbc517f7740c4be45ff
Thais <thaisavilam@gmail.com>
2021-07-24 00:17:42 -0300

    unificando passo 1 e implementando passo 2 do montador

--- include/montador.h
+++ include/montador.h
@@ -10,53 +10,30 @@ using namespace std;
 //definitions
 #define COMMENT_DELIMITER ";"
 #define LABEL_DELIMITER ":"
-#define MEM_SIZE "1000"     //maximum memory used (first number after MV-EXE)
+#define MEM_SIZE "1000"
 
-//classes
-// class Symbol {
-//     public:
-//     string label;
-//     string command;
-//     Symbol(string labelValue, string commandValue) {
-//         label = labelValue;
-//         command = commandValue;
-//     }
-// };
-
-
-
-// enum Symbol {
-//     HALT = 0,  // Stop the program.
-//     LOAD = 1,  // Reg[R] <- Mem[M + PC]
-//     STORE = 2, // Mem[M + PC] <- Reg[R]
-//     READ = 3,  // Reg[R] <- input
-//     WRITE = 4, // output <- Reg[R]
-//     COPY = 5,  // Reg[R1] <- Reg[R2], update PSW
-//     PUSH = 6,  // SP <- SP - 1; Mem[SP] <- Reg[R]
-//     POP = 7,   // Reg[R] <- Mem[SP]; SP <- SP + 1;
-//     ADD = 8,   // Reg[R1] <- Reg[R1] + Reg[R2], update PSW
-//     SUB = 9,   // Reg[R1] <- Reg[R1] - Reg[R2], update PSW
-//     MUL = 10,  // Reg[R1] <- Reg[R1] * Reg[R2], update PSW
-//     DIV = 11,  // Reg[R1] <- Reg[R1] / Reg[R2], update PSW
-//     MOD = 12,  // Reg[R1] <- Reg[R1] % Reg[R2], update PSW
-//     AND = 13,  // Reg[R1] <- Reg[R1] & Reg[R2], update PSW
-//     OR = 14,   // Reg[R1] <- Reg[R1] | Reg[R2], update PSW
-//     NOT = 15,  // Reg[R1] <- ~Reg[R1], update PSW
-//     JUMP = 16, // PC <- PC + M
-//     JZ = 17,   // If PSW[0] == 1, PC <- PC + M
-//     JN = 18,   // If PSW[1] == 1, PC <- PC + M
-//     CALL = 19, // SP <- SP - 1; Mem[SP] <- PC; PC <- PC + M
-//     RET = 20,  // PC <- Mem[SP]; SP <- SP + 1
-// };
-
-
-// enum Reg {
-//     R0 = 0, 
-//     R1 = 1,  
-//     R2 = 2,
-//     R3 = 3,  
-// };
-
+class StringIntHash {
+    private:
+        map<string, int>::iterator it;
+        map<string, int> hash;
+    public:
+        StringIntHash() {}
+        StringIntHash(map<string, int> initialHash) {
+            hash = initialHash;
+        }
+        void addToHash(string symbol, int value) {
+            hash.insert(pair<string,int>(symbol, value));
+        }
+        int getCorrespondent(string command) {
+            it = hash.find(command);
+            if(it == hash.end()) {
+                return -1;
+            }
+            else {
+                return it->second;
+            }
+        }
+};
 
 class Program {
     public:
@@ -71,31 +48,60 @@ class Program {
         }
 };
 
-
 //INUTIL POR ENQUANTO: class to accommodate an instruction
-class Instruction {
-    public:
-	    int      opcode;
-        vector<int> operands; // 0, 1, or 2 operands, depending on the symbol <sym>.
-        int         mem_addr;
+// class Instruction {
+//     public:
+// 	    int      opcode;
+//         vector<int> operands; // 0, 1, or 2 operands, depending on the symbol <sym>.
+//         int         mem_addr;
 
 
-        void setInstruction (int opcode, vector<int> operands, int mem_addr ){
-            this->opcode =      opcode;
-            this->operands =    operands;
-            this->mem_addr =    mem_addr;
-        }
-};
-
-
-
+//         void setInstruction (int opcode, vector<int> operands, int mem_addr ){
+//             this->opcode =      opcode;
+//             this->operands =    operands;
+//             this->mem_addr =    mem_addr;
+//         }
+// };
 
 //assembler functions
-int assemble(char *fileName);  					//called in main
-Program readProgram(ifstream &programFile);		//read a file (programFile) and return a  Program instance
+int assemble(char *fileName);
+void firstStep(ifstream &programFile, Program &program, StringIntHash &symbolHash);
+void secondStep(Program &program, StringIntHash &symbolHash);
 vector<string> getMeaningfulVec(string &line);	
-int firstStep ( const Program program, const map<string, int> Hash, map<string, int> &Sym );
-bool    isNumber(const string& str);
-vector<int> secondStep (const Program program,  map<string, int> Hash,  map<string, int> Sym);
+
+const map<string, int> Instructions {
+    {"HALT",    0}, // Stop the program
+    {"LOAD",    1}, // Reg[R] <- Mem[M + PC]
+    {"STORE",   2}, // Mem[M + PC] <- Reg[R]
+    {"READ",    3}, // Reg[R] <- input
+    {"WRITE",   4}, // output <- Reg[R]
+    {"COPY",    5}, // Reg[R1] <- Reg[R2], update PSW
+    {"PUSH",    6}, // SP <- SP - 1; Mem[SP] <- Reg[R]
+    {"POP",     7}, // Reg[R] <- Mem[SP]; SP <- SP + 1;
+    {"ADD",     8}, // Reg[R1] <- Reg[R1] + Reg[R2], update PSW
+    {"SUB",     9}, // Reg[R1] <- Reg[R1] - Reg[R2], update PSW
+    {"MUL",    10}, // Reg[R1] <- Reg[R1] * Reg[R2], update PSW
+    {"DIV",    11}, // Reg[R1] <- Reg[R1] / Reg[R2], update PSW
+    {"MOD",    12}, // Reg[R1] <- Reg[R1] % Reg[R2], update PSW
+    {"AND",    13}, // Reg[R1] <- Reg[R1] & Reg[R2], update PSW
+    {"OR",     14}, // Reg[R1] <- Reg[R1] | Reg[R2], update PSW
+    {"NOT",    15}, // Reg[R1] <- ~Reg[R1], update PSW
+    {"JUMP",   16}, // PC <- PC + M
+    {"JZ",     17}, // If PSW[0] == 1, PC <- PC + M
+    {"JN",     18}, // If PSW[1] == 1, PC <- PC + M
+    {"CALL",   19}, // SP <- SP - 1; Mem[SP] <- PC; PC <- PC + M
+    {"RET",    20}, // PC <- Mem[SP]; SP <- SP + 1
+};
+
+//Pseudo-instructions
+#define PSEUDO_INST_WORD "WORD"
+#define PSEUDO_INST_END "END"
+
+const map<string, int> Registers {
+    {"R0",      0},
+    {"R1",      1},
+    {"R2",      2},
+    {"R3",      3}
+};
 
 #endif
--- src/montador.cpp
+++ src/montador.cpp
@@ -1,96 +1,110 @@
 #include <iostream>
 #include <string>
 #include "montador.h"
-#include <map>
 
-void printProgram(Program &program) {
+StringIntHash instructionHash(Instructions);
+StringIntHash registerHash(Registers);
+
+// void printProgram(Program &program) {
+//     string command;
+//     cout << endl << "--------------------------" << endl;
+//     cout << "Programa" << endl;
+//     cout << "--------------------------" << endl;
+//     for(size_t i = 0; i < program.amountOfLines; i++) {
+//         for(size_t j = 0; j < program.lines[i].size(); j++) {
+//             command = command + " | " + program.lines[i][j];
+//         }
+//         cout << i << command + " | " << endl;
+//         command.clear();
+//     }
+//     return;
+// }
+
+// void printSymbolHash(SymbolHash &symbolHash) {
+//     string command;
+//     cout << endl << "--------------------------" << endl;
+//     cout << "Tabela de Simbolos" << endl;
+//     cout << "--------------------------" << endl;
+//     for(auto it = symbolHash.hash.cbegin(); it != symbolHash.hash.cend(); ++it)
+//     {
+//         std::cout << it->first << " : " << it->second << endl;
+//     }
+//     return;
+// }
+
+int assemble(char *fileName) {
+    ifstream programFile(fileName);
+    Program program;
+    StringIntHash symbolHash;
+    firstStep(programFile, program, symbolHash);
+    secondStep(program, symbolHash);
+  
+    // printProgram(program);
+    // printSymbolHash(symbolHash);
+
+    programFile.close();
+    return 0;
+}
+
+void firstStep(ifstream &programFile, Program &program, StringIntHash &symbolHash) {
+    string line;
+    string symbol;
     string command;
-    for(size_t i = 0; i < program.amountOfLines; i++) {
-        for(size_t j = 0; j < program.lines[i].size(); j++) {
-            command = command + " | " + program.lines[i][j];
+    while(getline(programFile, line)) {
+        vector<string> meaningfulVec = getMeaningfulVec(line);
+        if(meaningfulVec.size() > 0 && meaningfulVec[0].back() == *LABEL_DELIMITER) {
+            symbol = meaningfulVec[0].substr(0, meaningfulVec[0].size()-1);
+            meaningfulVec.erase(meaningfulVec.begin());
         }
-        cout << command + " | " << endl;
+        if(meaningfulVec.size() > 0) {
+            command = meaningfulVec[0];
+            if(command == PSEUDO_INST_WORD && !symbol.empty() && meaningfulVec.size() > 1) {
+                program.addLine({meaningfulVec[1]});
+                symbolHash.addToHash(symbol, program.amountOfLines);
+            }
+            else if(command == PSEUDO_INST_END) {
+                return;
+            }
+            else if(instructionHash.getCorrespondent(command) >= 0) {
+                program.addLine(meaningfulVec);
+                if(!symbol.empty()) {
+                    symbolHash.addToHash(symbol, program.amountOfLines);
+                }
+            }
+        }
+        symbol.clear();
         command.clear();
     }
     return;
 }
 
- ////////////////////////ASSEMBLE//////////////////////////
-int assemble(char *fileName) {
-    ifstream programFile(fileName);
-    Program program = readProgram(programFile);
-
-const map<string, int> known_hash {
-        {"HALT" , 0},  // Stop the program.
-        {"LOAD" , 1},  // Reg[R] <- Mem[M + PC]
-        {"STORE" , 2}, // Mem[M + PC] <- Reg[R]
-        {"READ" , 3},  // Reg[R] <- input
-        {"WRITE" , 4}, // output <- Reg[R]
-        {"COPY" , 5},  // Reg[R1] <- Reg[R2], update PSW
-        {"PUSH" , 6},  // SP <- SP - 1; Mem[SP] <- Reg[R]
-        {"POP" , 7},   // Reg[R] <- Mem[SP]; SP <- SP + 1;
-        {"ADD" , 8},   // Reg[R1] <- Reg[R1] + Reg[R2], update PSW
-        {"SUB" , 9},   // Reg[R1] <- Reg[R1] - Reg[R2], update PSW
-        {"MUL" , 10},  // Reg[R1] <- Reg[R1] * Reg[R2], update PSW
-        {"DIV" , 11},  // Reg[R1] <- Reg[R1] / Reg[R2], update PSW
-        {"MOD" , 12},  // Reg[R1] <- Reg[R1] % Reg[R2], update PSW
-        {"AND" , 13},  // Reg[R1] <- Reg[R1] & Reg[R2], update PSW
-        {"OR" , 14},   // Reg[R1] <- Reg[R1] | Reg[R2], update PSW
-        {"NOT" , 15},  // Reg[R1] <- ~Reg[R1], update PSW
-        {"JUMP" , 16}, // PC <- PC + M
-        {"JZ" , 17},   // If PSW[0] == 1, PC <- PC + M
-        {"JN" , 18},   // If PSW[1] == 1, PC <- PC + M
-        {"CALL" , 19}, // SP <- SP - 1; Mem[SP] <- PC; PC <- PC + M
-        {"RET" , 20},  // PC <- Mem[SP]; SP <- SP + 1
-        {"R0",0},
-        {"R1",1},
-        {"R2",2},
-        {"R3",3},
-        {"WORD",0},
-        {"END",0},
-    };
-
-    map<string, int> Symb_hash; //tabela de symbolos a ser populada pela primeira etapa
-     firstStep(program, known_hash, Symb_hash);
-
-     //cout<<Symb_hash["test:"]<<endl;
-    
-    vector<int> finalBin = secondStep(program, known_hash, Symb_hash);
-    cout << finalBin[2]<<endl;
-
-  
-    //printProgram(program);
-
-
-
-    
- 
-
-
-  
-
-
-
-
-
-
-
-    programFile.close();
-    return 0;
-}
-//////////////////////////////////////////////////////////
-
-
-Program readProgram(ifstream &programFile) {
-    string line;
-    Program program;
-    while(getline(programFile, line)) {
-        vector<string> meaningfulVec = getMeaningfulVec(line);
-        if(meaningfulVec.size() > 0) {
-            program.addLine(meaningfulVec);
+void secondStep(Program &program, StringIntHash &symbolHash) {
+    string command;
+    int correspondent;
+    vector<int> machineCommands;
+    int machineCommandAmmount = 0;
+    for(size_t i = 0; i < program.amountOfLines; i++) {
+        for(size_t j = 0; j < program.lines[i].size(); j++) {
+            command = program.lines[i][j];
+            correspondent = instructionHash.getCorrespondent(command);
+            if(correspondent < 0) {
+                correspondent = registerHash.getCorrespondent(command);
+            }
+            if(correspondent < 0) {
+                correspondent = symbolHash.getCorrespondent(command);
+            }
+            if(correspondent >= 0) {
+                machineCommands.push_back(correspondent);
+                machineCommandAmmount++;
+                cout << correspondent << " ";
+            }
+            else {
+                machineCommands.push_back(atoi(command.c_str()));
+                machineCommandAmmount++;
+                cout << atoi(command.c_str()) << " ";
+            }
         }
     }
-    return program;
 }
 
 vector<string> getMeaningfulVec(string &line) {
@@ -121,83 +135,39 @@ vector<string> getMeaningfulVec(string &line) {
     return meaningfulVec;
 }
 
-//seeks for some undetermined label/sysmbol and creates a symbol table
-//it receives a program, a a dictionary with all commands (reg/opcodes) and a Symbol Hash of user diffined sysmbols.
-int firstStep ( const Program program, const map<string, int> Hash, map<string, int> &Sym ){
-    Instruction instAux;
-    map<string, int>::iterator it;
-    string item_a; //iterator for Sym
-    int instLine = 0;
+// vector<int> secondStep (const Program program, map<string, int> Hash,  map<string, int> Sym){
+//     int instLine = 0;   //current isntruction line
+//     bool WORDappear = 0 ; //accommodates the memory position of WORD storage
+//     vector<int> finalBin; //vector with the final decial numbers to be outputed
+//     map<string, int>::iterator it;
 
-    for(size_t i = 0; i<program.amountOfLines;  i++){
-        instLine++;
-        for(auto item: program.lines[i]){
-            
-            //identificando  comandos indefinidos;
-            
-            if((Hash.find(item) == Hash.end()) & (!isNumber(item))) { //unknown command
+//     for(size_t i = 0; i<program.amountOfLines;  i++){
+//         instLine++;
+//         for(auto item: program.lines[i]){
+//             if(item == "END") return finalBin;//stops if END is found
 
-                if(!(item.back()== *LABEL_DELIMITER)) {item_a = item + *LABEL_DELIMITER;} else {item_a = item;}; //adds ':' to item's end
+
+//             if((Hash.find(item) == Hash.end()) & (!isNumber(item)) & (item.back() != ':')) { //found a Symbol. substitute with its memory position 
+
+//                 it  = Sym.find(item + ':');
+//                 finalBin.push_back(it->second);
                 
-                it  = Sym.find(item_a);
-                if(it == Sym.end()){ //the unknonw command does NOT exist
-                   Sym.insert(pair<string,int>(item_a,*MEM_SIZE)); 
-                } 
-            else{//the unknonw command DOES exist
-                    it->second = instLine;
-                }
-            }
-            else {} //known command
-        }
-    }
-    return 0;
-}
+//             } 
+//             else if(item == "WORD"){
+//                 WORDappear = true;
+//             }
+//             else if(isNumber(item) & (WORDappear)){
+//                 finalBin.push_back(stoi(item));
+//                 WORDappear = false;
+//             }
+//         }
+//     }
+//     return finalBin;
+// }
 
-
-
-vector<int> secondStep (const Program program, map<string, int> Hash,  map<string, int> Sym){
-    int instLine = 0;   //current isntruction line
-    bool WORDappear = 0 ; //accommodates the memory position of WORD storage
-    vector<int> finalBin; //vector with the final decial numbers to be outputed
-    map<string, int>::iterator it;
-
-    for(size_t i = 0; i<program.amountOfLines;  i++){
-        instLine++;
-        for(auto item: program.lines[i]){
-            if(item == "END") return finalBin;//stops if END is found
-
-
-            if((Hash.find(item) == Hash.end()) & (!isNumber(item)) & (item.back() != ':')) { //found a Symbol. substitute with its memory position 
-
-                it  = Sym.find(item + ':');
-                finalBin.push_back(it->second);
-                
-            } 
-            else if(item == "WORD"){
-                WORDappear = true;
-            }
-            else if(isNumber(item) & (WORDappear)){
-                finalBin.push_back(stoi(item));
-                WORDappear = false;
-            }
-        }
-    }
-    return finalBin;
-}
-
-//check se uma string contém um número
-bool isNumber(const string& str)
-{
-    for (char const &c : str) {
-        if (std::isdigit(c) == 0) return false;
-    }
-    return true;
-}
-
-void generateInstructions(){
-
-
-
-
-
-}
+// bool isNumber(const string &str) {
+//     for(const char &c : str) {
+//         if (isdigit(c) != 0) return false;
+//     }
+//     return true;
+// }
